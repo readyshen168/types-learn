1ee3a228c2c169732be6cf9d6274d08d65d85e34
Ubuntu <ubuntu@localhost.localdomain>
2025-12-13 22:59:12 +0800

    利用泛型算法，将两个函数中相同的算法的类型和检查的条件参数化

--- dist/demo1_2_4.js
+++ dist/demo1_2_4.js
@@ -2,19 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.findFirstNegativeNumber = findFirstNegativeNumber;
 exports.findFirstOneCharacterString = findFirstOneCharacterString;
-function findFirstNegativeNumber(numbers) {
-    for (var _i = 0, numbers_1 = numbers; _i < numbers_1.length; _i++) {
-        var num = numbers_1[_i];
-        if (num < 0) {
-            return num;
+function first(elements, p) {
+    for (var _i = 0, elements_1 = elements; _i < elements_1.length; _i++) {
+        var e = elements_1[_i];
+        if (p(e)) {
+            return e;
         }
     }
 }
+function findFirstNegativeNumber(numbers) {
+    return first(numbers, function (num) { return num < 0; });
+}
 function findFirstOneCharacterString(strings) {
-    for (var _i = 0, strings_1 = strings; _i < strings_1.length; _i++) {
-        var str = strings_1[_i];
-        if (str.length === 1) {
-            return str;
-        }
-    }
+    return first(strings, function (str) { return str.length === 1; });
 }
--- src/demo1_2_4.ts
+++ src/demo1_2_4.ts
@@ -1,16 +1,16 @@
-export function findFirstNegativeNumber(numbers: number[]): number | undefined{
-    for(let num of numbers){
-        if(num < 0){
-            return num;
+function first<T>(elements: T[], p: (elem: T) => boolean ): T | undefined{
+    for(let e of elements){
+        if(p(e)){
+            return e;
         }
     }
 }
 
-export function findFirstOneCharacterString(strings: string[]): string | undefined{
-    for(let str of strings){
-        if(str.length === 1){
-            return str;
-        }
-    }
 
+export function findFirstNegativeNumber(numbers: number[]): number | undefined{
+    return first(numbers, num => num < 0 );
+}
+
+export function findFirstOneCharacterString(strings: string[]): string | undefined{
+    return first(strings, str => str.length ===1 );
 }
